77170b790ea1ebcb0ebf8a9f0611d49b8a41a4cc
wesley309004-dot <wesley309004@gmail.com>
2026-07-08 12:10:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from playwright.sync_api import sync_playwright
 TOKEN = os.environ["TELEGRAM_TOKEN"]
 CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
 
-def send_message(text):
+def send(text):
     requests.post(
         f"https://api.telegram.org/bot{TOKEN}/sendMessage",
         data={
@@ -14,28 +14,29 @@ def send_message(text):
         }
     )
 
-try:
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=True)
+    page = browser.new_page()
 
-        page = browser.new_page()
-
-        page.goto(
-            "https://www.vietjetair.com/zh-TW",
-            timeout=60000
-        )
-
-        title = page.title()
-
-        send_message(
-            "✈️ Vietjet瀏覽器測試成功\n"
-            f"頁面：{title}"
-        )
-
-        browser.close()
-
-except Exception as e:
-    send_message(
-        "❌ Vietjet測試失敗\n"
-        + str(e)
+    page.goto(
+        "https://www.vietjetair.com/zh-TW",
+        timeout=60000
     )
+
+    inputs = page.locator("input")
+
+    result = "找到輸入框數量：" + str(inputs.count()) + "\n\n"
+
+    for i in range(inputs.count()):
+        try:
+            result += (
+                f"{i}: "
+                + inputs.nth(i).get_attribute("placeholder")
+                + "\n"
+            )
+        except:
+            pass
+
+    send(result)
+
+    browser.close()
