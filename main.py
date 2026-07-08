6a3382dd8cbcc612d2309f2a8ecad4192392f8a2
wesley309004-dot <wesley309004@gmail.com>
2026-07-08 11:42:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,41 @@ import requests
 TOKEN = os.environ["TELEGRAM_TOKEN"]
 CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
 
-message = "✈️ Vietjet監控系統測試成功！"
+url = "https://www.vietjetair.com/en/pages/promotions"
 
-url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+keywords = [
+    "Japan",
+    "Sale",
+    "Promotion",
+    "Flash Sale",
+    "Discount"
+]
 
-requests.post(
-    url,
-    data={
-        "chat_id": CHAT_ID,
-        "text": message
-    }
-)
+try:
+    r = requests.get(url, timeout=20)
+    text = r.text.lower()
+
+    found = []
+
+    for k in keywords:
+        if k.lower() in text:
+            found.append(k)
+
+    if found:
+        message = (
+            "✈️ Vietjet 促銷偵測！\n\n"
+            "發現關鍵字：\n"
+            + ", ".join(found)
+            + "\n\n請立即查看官網"
+        )
+
+        requests.post(
+            f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+            data={
+                "chat_id": CHAT_ID,
+                "text": message
+            }
+        )
+
+except Exception as e:
+    print(e)
