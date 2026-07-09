1844c5a7baa45b0eab92efc9a980d15a36bf2c91
wesley309004-dot <wesley309004@gmail.com>
2026-07-09 19:27:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,35 +20,9 @@ def send(text):
 
 
 
-def real_click(page, locator):
-
-    box = locator.bounding_box()
-
-    if box:
-
-        x = box["x"] + box["width"] / 2
-        y = box["y"] + box["height"] / 2
-
-        page.mouse.move(
-            x,
-            y
-        )
-
-        page.mouse.down()
-
-        page.wait_for_timeout(200)
-
-        page.mouse.up()
-
-        return True
-
-    return False
-
-
-
 try:
 
-    result = "=== Vietjet目的地測試 ===\n\n"
+    result = "=== 目的地分類測試 ===\n\n"
 
 
     with sync_playwright() as p:
@@ -74,8 +48,6 @@ try:
 
 
 
-        # Cookie
-
         try:
 
             page.locator("h5").filter(
@@ -89,45 +61,6 @@ try:
         except:
 
             pass
-
-
-
-        # 出發地
-
-        page.get_by_text(
-            "出發地點",
-            exact=True
-        ).click(
-            force=True,
-            timeout=5000
-        )
-
-
-        page.wait_for_timeout(3000)
-
-
-
-        # TPE
-
-        airport = page.locator(
-            "div.jss830"
-        ).filter(
-            has_text="桃園國際機場"
-        ).first
-
-
-
-        real_click(
-            page,
-            airport
-        )
-
-
-        page.wait_for_timeout(3000)
-
-
-
-        result += "TPE完成\n"
 
 
 
@@ -146,78 +79,13 @@ try:
 
 
 
-        # 找 DAD
-
-        dad = page.locator(
-            "div.jss829"
-        ).filter(
-            has_text="DAD"
-        ).first
+        body = page.locator(
+            "body"
+        ).inner_text()
 
 
 
-        count = dad.count()
-
-
-        result += (
-            "DAD數量:"
-            + str(count)
-            + "\n"
-        )
-
-
-
-        if count > 0:
-
-
-            parent = dad.locator(
-                "xpath=ancestor::div[contains(@class,'MuiBox-root')][1]"
-            )
-
-
-            result += (
-                "找到:\n"
-                + parent.evaluate(
-                    "(e)=>e.outerHTML"
-                )[:300]
-                + "\n"
-            )
-
-
-
-            real_click(
-                page,
-                dad
-            )
-
-
-            page.wait_for_timeout(3000)
-
-
-            result += "已點擊DAD\n"
-
-
-
-        else:
-
-            result += "找不到DAD\n"
-
-
-
-        # 檢查
-
-        remain = page.locator(
-            "div.jss829"
-        ).filter(
-            has_text="DAD"
-        ).count()
-
-
-        result += (
-            "剩餘DAD:"
-            + str(remain)
-            + "\n"
-        )
+        result += body[:3000]
 
 
 
